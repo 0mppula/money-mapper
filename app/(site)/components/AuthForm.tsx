48b980976ca4f3c78746613f726ed863fe47d791
Omar Kraidié <omppula_1@hotmail.com>
2023-08-02 14:13:11 +0300

    Added form auth form toggler.

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,12 +1,21 @@
 'use client';
 
 import { ButtonWithIcon } from '@/components/ButtonWithIcon';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
 
 const AuthForm = () => {
+	const [variant, setVariant] = useState<'LOGIN' | 'REGISTER'>('REGISTER');
 	const [googleIsLoading, setGoogleIsLoading] = useState(false);
 	const [githubIsLoading, setGithubIsLoading] = useState(false);
+
+	const toggleVariant = useCallback(() => {
+		if (variant === 'LOGIN') {
+			setVariant('REGISTER');
+		} else {
+			setVariant('LOGIN');
+		}
+	}, [variant]);
 
 	const socialAction = (provider: string) => {
 		if (provider === 'google') {
@@ -21,14 +30,17 @@ const AuthForm = () => {
 	};
 
 	return (
-		<form className="space-y-4 mt-4 lg:mt-8 bg-white px-4 py-6 shadow rounded-lg sm:px-10 w-full sm:max-w-lg max-w-md">
+		<form
+			className="gap-4 flex flex-col mt-4 lg:mt-8 bg-white px-4 py-6 shadow rounded-lg sm:px-10 w-full sm:max-w-lg max-w-md"
+			onSubmit={(e) => e.preventDefault()}
+		>
 			<ButtonWithIcon
 				loading={googleIsLoading}
 				disabled={googleIsLoading || githubIsLoading}
 				type="button"
 				className="w-full"
 				icon={FaGoogle}
-				label="Continue with Google"
+				label={variant === 'LOGIN' ? 'Continue with Google' : 'Create an account'}
 				onClick={() => socialAction('google')}
 			/>
 
@@ -38,9 +50,19 @@ const AuthForm = () => {
 				type="button"
 				className="w-full"
 				icon={FaGithub}
-				label="Continue with Github"
+				label={variant === 'LOGIN' ? 'Continue with Github' : 'Create an account'}
 				onClick={() => socialAction('github')}
 			/>
+
+			<hr />
+
+			<div className="flex gap-2">
+				{variant === 'LOGIN' ? 'New to Money Mapper?' : 'Already have an account?'}
+
+				<button onClick={toggleVariant} className="underline cursor-pointer">
+					{variant === 'LOGIN' ? 'Create an account' : 'Login'}
+				</button>
+			</div>
 		</form>
 	);
 };
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,7 @@ import AuthForm from './components/AuthForm';
 export default function Home() {
 	return (
 		<div className="container flex min-h-full flex-col justify-center py-12 items-center">
-			<TypographyH1 center>Sign in or create an account </TypographyH1>
+			<TypographyH1 center>Sign in to your account</TypographyH1>
 
 			<AuthForm />
 		</div>
