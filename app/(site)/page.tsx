98eb9b3b3a47b348aed40ac413188dcfbecfed62
Omar Kraidié <omppula_1@hotmail.com>
2023-08-06 12:52:19 +0300

    Fixed login page for small screens.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,7 @@ import AuthForm from './components/AuthForm';
 
 export default function Home() {
 	return (
-		<div className="container flex min-h-full flex-col justify-center py-12 items-center">
+		<div className="container flex min-h-full flex-col items-center justify-start sm:justify-center py-12">
 			<TypographyH1 center>Sign in to your account</TypographyH1>
 
 			<AuthForm />
