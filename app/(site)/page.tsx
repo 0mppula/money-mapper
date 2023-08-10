1a24d078ae2aa63bde07b96db5138dfe7231c03f
Omar Kraidié <omppula_1@hotmail.com>
2023-08-10 10:10:12 +0300

    Updated login page.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,7 @@ import AuthForm from './components/AuthForm';
 
 export default function Home() {
 	return (
-		<div className="container flex flex-col items-center justify-start sm:justify-center pt-24 sm:pt-12 py-12 min-h-[calc(100vh-69px)] sm:pb-[117px]">
+		<div className="container flex flex-col items-center justify-center pb-[117px] py-12 min-h-[calc(100vh-69px)]">
 			<TypographyH1 center>Sign in to your account</TypographyH1>
 
 			<AuthForm />
