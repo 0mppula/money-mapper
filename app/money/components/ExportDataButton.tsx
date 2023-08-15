10454446addffdc5428a924db50a8f71f5900559
Omar Kraidié <omppula_1@hotmail.com>
2023-08-15 13:59:08 +0300

    Added .xlsx data exporting.

--- app/money/components/ExportDataButton.tsx
+++ app/money/components/ExportDataButton.tsx
@@ -1,11 +1,18 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/use-toast';
 import { FinancialRecord } from '@prisma/client';
 import { useQuery } from '@tanstack/react-query';
+import { format } from 'date-fns';
+import xlsx, { IJsonSheet, ISettings } from 'json-as-xlsx';
 import { Download } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { ImSpinner8 } from 'react-icons/im';
 import { getFinancialRecords } from './FinancialRecordTable';
 
 const ExportDataButton = () => {
+	const [canGenerateXlsx, setCanGenerateXlsx] = useState(true);
+
 	const { data, isLoading, isError } = useQuery<
 		(FinancialRecord & { netWorth: number; totalAssets: number })[]
 	>({
@@ -13,10 +20,110 @@ const ExportDataButton = () => {
 		queryFn: getFinancialRecords,
 	});
 
+	useEffect(() => {
+		let rateLimit: NodeJS.Timeout;
+
+		if (!canGenerateXlsx) {
+			rateLimit = setTimeout(() => {
+				setCanGenerateXlsx(true);
+			}, 1000);
+		}
+
+		return () => {
+			clearTimeout(rateLimit);
+		};
+	}, [canGenerateXlsx]);
+
+	const downloadToExcel = async () => {
+		const workbook = await new Promise((resolve) => {
+			setCanGenerateXlsx(false);
+			try {
+				const columns: IJsonSheet[] = [
+					{
+						sheet: 'Money Breakdown',
+						columns: [
+							{
+								label: 'Date', // @ts-ignore
+								value: (row) => new Date(row?.date),
+							},
+							{
+								label: 'Gross Income YTD',
+								value: (row) => row?.grossIncomeYtd,
+								format: '0.00',
+							},
+							{
+								label: 'Taxes Paid YTD',
+								value: (row) => row?.taxesPaidYtd,
+								format: '0.00',
+							},
+							{
+								label: 'Assets Ex Cash',
+								value: (row) => row?.assetsExCash,
+								format: '0.00',
+							},
+							{
+								label: 'Total Cash',
+								value: (row) => row?.cash,
+								format: '0.00',
+							},
+							{
+								label: 'Total Assets',
+								value: (row) => row?.totalAssets,
+								format: '0.00',
+							},
+							{
+								label: 'Debt',
+								value: (row) => row?.debt,
+								format: '0.00',
+							},
+							{
+								label: 'Net Worth',
+								value: (row) => row?.netWorth,
+								format: '0.00',
+							},
+						], // @ts-ignore
+						content: data,
+					},
+				];
+
+				const settings: ISettings = {
+					fileName: `money-breakdown-${format(new Date(), 'yyyy-MM-dd')}`,
+				};
+
+				resolve(xlsx(columns, settings));
+			} catch (error) {
+				toast({
+					description: 'Error exporting data to excel',
+					variant: 'destructive',
+				});
+			} finally {
+				toast({
+					description: 'Data exported in excel format',
+				});
+			}
+		});
+
+		return workbook;
+	};
+
 	return (
-		<Button disabled={isLoading || isError} variant="outline">
-			<Download className="h-[1.125rem] w-[1.125rem] mr-2" /> .csv
-			<span className="sr-only">Export data as csv</span>
+		<Button
+			disabled={isLoading || isError || !canGenerateXlsx}
+			onClick={downloadToExcel}
+			suppressHydrationWarning
+			variant="outline"
+		>
+			{!canGenerateXlsx ? (
+				<ImSpinner8 className="h-4 w-4 mr-2 animate-spin" />
+			) : (
+				<Download className="h-[1.125rem] w-[1.125rem] mr-2" />
+			)}
+			{!canGenerateXlsx ? (
+				<span className="sr-only">Loading...</span>
+			) : (
+				<span className="sr-only">Export data as xlsx</span>
+			)}{' '}
+			.xlsx
 		</Button>
 	);
 };
